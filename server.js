f3d9595dd5c754cf09f1019b1a0e760116b6bb9d
Courtney Owen <cowen@microsoft.com>
2019-11-11 10:40:17 -0800

    Remove server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-const sso = require('./node_modules/office-addin-sso/lib/server');
-require('dotenv').config();
-
-const ssoInstance = new sso.SSOService('./manifest.xml');
-ssoInstance.startSsoService();
