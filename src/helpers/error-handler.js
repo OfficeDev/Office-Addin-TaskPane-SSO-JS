92058521f1f3ef4a883db08de514d853f805bb50
Rick Kirkham <rickeeee@msn.com>
2024-06-12 13:59:04 -0700

    make uglier so prettier will stop wanting trailing comma in function parameters

--- src/helpers/error-handler.js
+++ src/helpers/error-handler.js
@@ -7,22 +7,16 @@ export function handleClientSideErrors(error) {
       // No one is signed into Office. If the add-in cannot be effectively used when no one
       // is logged into Office, then the first call of getAccessToken should pass the
       // `allowSignInPrompt: true` option.
-      showMessage(
-        "No one is signed into Office. But you can use many of the add-ins functions anyway. If you want to log in, press the Get OneDrive File Names button again."
-      );
+      showMessage("No one is signed into Office. But you can use many of the add-ins functions anyway. If you want to log in, press the Get OneDrive File Names button again.");
       return invokeFallBackDialog;
     case 13002:
       // The user aborted the consent prompt. If the add-in cannot be effectively used when consent
       // has not been granted, then the first call of getAccessToken should pass the `allowConsentPrompt: true` option.
-      showMessage(
-        "You can use many of the add-ins functions even though you have not granted consent. If you want to grant consent, press the Get OneDrive File Names button again."
-      );
+      showMessage("You can use many of the add-ins functions even though you have not granted consent. If you want to grant consent, press the Get OneDrive File Names button again.");
       return invokeFallBackDialog;
     case 13006:
       // Only seen in Office on the Web.
-      showMessage(
-        "Office on the Web is experiencing a problem. Please sign out of Office, close the browser, and then start again."
-      );
+      showMessage("Office on the Web is experiencing a problem. Please sign out of Office, close the browser, and then start again.");
       return invokeFallBackDialog;
     case 13008:
       // Only seen in Office on the Web.
--- src/helpers/fallbackauthdialog.js
+++ src/helpers/fallbackauthdialog.js
@@ -94,9 +94,7 @@ function handleResponse(response) {
     localStorage.setItem("loggedIn", "yes");
   } else {
     console.log("token type is:" + response.tokenType);
-    Office.context.ui.messageParent(
-      JSON.stringify({ status: "success", result: response.accessToken, accountId: response.account.homeAccountId })
-    );
+    Office.context.ui.messageParent(JSON.stringify({ status: "success", result: response.accessToken, accountId: response.account.homeAccountId }));
   }
 }
 
